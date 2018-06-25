7a3a66e94656b9f559daa2b1c0e344ecc2d0f22b
Vlad <you@ee.com>
2018-06-25 22:48:04 +0500

    3

--- santehvent/catalog/views.py
+++ santehvent/catalog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.template import loader
 
 from django.http import HttpResponse
+from .models import good
 
 
 def index(request):
-    return HttpResponse("Hello, world. ")
+    goods_list = []
+    other_arr = []
+
+    goods_list = good.objects.order_by('-qoodName')[:5]
+
+    template = loader.get_template('catalog/index.html')
+    context = {
+        'goods_list': goods_list, 'other': other_arr,
+    }
+    return HttpResponse(template.render(context, request))
