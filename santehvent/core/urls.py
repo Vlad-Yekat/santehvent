5e8c59d6ece1b2032e6cbac408e68c77ee43f69a
Vladislav Rybin <vladislavi@mail.ru>
2020-04-16 12:17:38 +0500

    ref

--- santehvent/core/urls.py
+++ santehvent/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import item_list
+from .views import HomeView, ItemDetailView
 
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('', HomeView.as_view(), name='home'),
 ]
--- santehvent/core/views.py
+++ santehvent/core/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Item
 
 
-def item_list(request):
+def products(request):
     context = {
         'items': Item.objects.all()
     }
-    return render(request, 'home-page.html', context)
+    return render(request, "products.html", context)
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"
